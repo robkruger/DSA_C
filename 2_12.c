e3c215b71cccad485f3581c081d64683a33c35f7
Rob Kruger <4958@praedinius.nl>
2022-10-10 21:34:57 +0200

    small changes

--- 2_12.c
+++ 2_12.c
@@ -8,23 +8,23 @@ int main (void){
     if(number < 2){
         printf("%i is not a prime number", number);
         return 0;
-    } else {
-        int i = 2;
-        int result = 1;
-        for(int i = 2; i <= sqrt(number); i++){
-            if((i * i) <= number){
-                if(number % i == 0){
-                    result = 0;
-                    break;
-                }
+    }
+    
+    int i = 2;
+    int result = 1;
+    for(int i = 2; i <= sqrt(number); i++){
+        if((i * i) <= number){
+            if(number % i == 0){
+                result = 0;
+                break;
             }
         }
-        if(!result){
-            printf("%i is not a prime number", number);
-            return 0;
-        }
-        printf("%i is a prime number", number);
     }
+    if(!result){
+        printf("%i is not a prime number", number);
+        return 0;
+    }
+    printf("%i is a prime number", number);
     
     return 0;
 }
