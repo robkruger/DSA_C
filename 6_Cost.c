1b93300d4c37dda7a8a3a663a91a595a2798a452
Rob Kruger <4958@praedinius.nl>
2022-10-11 19:39:24 +0200

    6_Cost finished

--- 6_Cost.c
+++ 6_Cost.c
@@ -1,1 +1,34 @@
-WIP
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%i", &n);
+
+    double prices[100];
+
+    for(int i = 0; i < n; i++){
+        scanf("%lf", &prices[i]);
+    }
+
+    int m;
+    scanf("%i", &m);
+
+    double amounts[100][100];
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%lf", &amounts[i][j]);
+        }
+    }
+
+    for(int i = 0; i < m; i++){
+        double sum = 0;
+        for(int j = 0; j < n; j++){
+            sum += amounts[i][j] * prices[j];
+        }
+        printf("%.2f\n", sum);
+
+    }
+
+    return 0;
+}
